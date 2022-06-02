adc336b090a9ae26966972234cce485837c3bad7
Ruben Taelman <ruben.taelman@ugent.be>
2022-06-02 09:09:00 +0200

    Allow custom construction settings to be passed to runner

--- lib/CliRunner.ts
+++ lib/CliRunner.ts
@@ -1,20 +1,23 @@
 import type { ReadStream, WriteStream } from 'tty';
-import type { IComponentsManagerBuilderOptions } from 'componentsjs';
 import { ComponentsManager } from 'componentsjs';
+import type { IComponentsManagerBuilderOptions, IConstructionSettings } from 'componentsjs';
 import type { ValidationGenerator } from './ValidationGenerator';
 
 /**
  * Run function for starting the validation generator for a given config.
  * @param configPath - Path to a config.
  * @param properties - Components loader properties.
+ * @param constructionSettings - Settings for instantiation.
  */
 export const runConfig = async function(
   configPath: string,
   properties: IComponentsManagerBuilderOptions<ValidationGenerator>,
+  constructionSettings?: IConstructionSettings,
 ): Promise<void> {
   const manager = await ComponentsManager.build(properties);
   await manager.configRegistry.register(configPath);
-  const generator: ValidationGenerator = await manager.instantiate('urn:ldbc-snb-validation-generator:default');
+  const generator: ValidationGenerator = await manager
+    .instantiate('urn:ldbc-snb-validation-generator:default', constructionSettings);
   return await generator.generate();
 };
 
@@ -25,6 +28,7 @@ export const runConfig = async function(
  * @param stdout - Standard output stream.
  * @param stderr - Standard error stream.
  * @param properties - Components loader properties.
+ * @param constructionSettings - Settings for instantiation.
  */
 export const runCustom = function(
   args: string[],
@@ -32,6 +36,7 @@ export const runCustom = function(
   stdout: WriteStream,
   stderr: WriteStream,
   properties: IComponentsManagerBuilderOptions<ValidationGenerator>,
+  constructionSettings?: IConstructionSettings,
 ): void {
   (async(): Promise<void> => {
     if (args.length !== 1) {
@@ -44,7 +49,7 @@ Usage:
     const configPath = args[0];
 
     // Setup from config file
-    return await runConfig(configPath, properties);
+    return await runConfig(configPath, properties, constructionSettings);
   })().then((): void => {
     // Done
   }).catch(error => {
@@ -55,7 +60,7 @@ Usage:
 };
 
 /**
- * Run function for starting the server from the command line
+ * Run function for starting the validation-generator from the command line
  * @param moduleRootPath - Path to the module's root.
  */
 export const runCli = function(moduleRootPath: string): void {
